55818e3cf05f56d2bb2140f9abbda8bbaab6e5b8
geovanheew <geovanheewofc123@gmail.com>
2024-09-04 21:09:32 -0300

    abas

--- api/index.php
+++ api/index.php
@@ -1,12 +1,2 @@
 <?php 
-
-  $req = $_SERVER;
-
-switch ($_SERVER["REQUEST_METHOD"]) {
-    case 'GET':
-        echo 'GET';
-        break;
-    default:
-        echo 'seila';
-        break;
-}
+phpinfo();
