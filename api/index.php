2efe9dec156399552a6b1041a88836b6c3518bf6
geovanheew <geovanheewofc123@gmail.com>
2024-09-06 15:07:39 -0300

    ASD

--- api/index.php
+++ api/index.php
@@ -2,11 +2,10 @@
     $metodo = $_SERVER['REQUEST_METHOD'];
     echo $metodo;
 
-    require_once '../Model/Resposta.php';
+    require_once __DIR__.'/../Model/Resposta.php';
     
     switch ($metodo) {
         case 'GET':
-            
             break;
         case 'POST':
             break;
